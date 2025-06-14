8d1b2d57d644df33160e26c8a6a657af90d50016
“Tensor” <alifnurrokhman4@gmail.com>
2025-06-15 02:53:14 +0700

    content landingpage n update mainlayout

--- template-quasar-alif/src/layouts/MainLayout.vue
+++ template-quasar-alif/src/layouts/MainLayout.vue
@@ -1,37 +1,51 @@
 <template>
-  <q-layout view="lHh lpr lFf">
-    <!-- Header: sembunyi saat scroll ke bawah -->
-    <q-header reveal elevated>
-        <q-toolbar>
-          <q-btn flat round dense icon="menu" class="q-mr-sm" />
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
+  <q-layout view="lHh lpr lFh" class="column full-height">
+    <!-- Header -->
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-avatar>
+          <img src="https://cdn.quasar.dev/img/avatar.png">
+        </q-avatar>
+        <q-toolbar-title class="text-weight-bold q-ml-sm">
+          Portofolio Alif Nur Rokhman
+        </q-toolbar-title>
+        <q-btn round dense icon="account_circle" to="/ya" />
+        <q-btn round dense icon="account_circle" />
+        <q-btn round dense icon="account_circle" />
+      </q-toolbar>
+    </q-header>
 
-          <q-toolbar-title>Quasar Framework</q-toolbar-title>
-
-          <q-btn flat round dense icon="whatshot" />
-        </q-toolbar>
-      </q-header>
-
-    <!-- Page Content -->
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-
-    <!-- Footer: sembunyi saat scroll ke atas -->
-    <q-footer reveal elevated>
-        <q-toolbar>
-          <q-toolbar-title>Footer</q-toolbar-title>
-        </q-toolbar>
-      </q-footer>
+    <div class="column fit">
+      <q-page-container class="col">
+        <q-page class="q-pa-md">
+          <router-view />
+        </q-page>
+      </q-page-container>
+      <footer
+        v-if="showFooter"
+        class="bg-grey-3 text-dark q-pt-md q-pb-md"
+      >
+        <div class="text-center text-caption">
+          &copy; {{ new Date().getFullYear() }} Alif Rokhman. Dibuat dengan Quasar Framework.
+        </div>
+      </footer>
+    </div>
   </q-layout>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, computed } from 'vue'
+import { useRoute } from 'vue-router'
 
 export default defineComponent({
   name: 'MainLayout',
+  setup() {
+    const route = useRoute()
+
+    // Tampilkan footer hanya di halaman utama (landing page)
+    const showFooter = computed(() => route.path === '/')
+
+    return { showFooter }
+  }
 })
 </script>
--- template-quasar-alif/src/pages/IndexPage.vue
+++ template-quasar-alif/src/pages/IndexPage.vue
@@ -1,118 +1,146 @@
 <template>
-  <div class="q-pa-md">
-    <div class="q-col-gutter-md row items-start">
-      <div class="col-4">
-        Ratio: 16/9
-        <q-img
-          src="https://picsum.photos/500/300"
-          :ratio="16/9"
-        />
+  <div class="q-pa-sm" style="background-color: rgb(247, 247, 247);">
+    <div class="column items-center">
+      <div class="text-h6 q-mb-lg">
+        Selamat datang di Dashboard e-Program
       </div>
-
-      <div class="col-4">
-        Ratio: 1
-        <q-img
-          src="https://picsum.photos/500/300"
-          :ratio="1"
-        />
+      <div class="text-h6 q-mb-lg">
+        Selamat datang di Dashboard e-Program
       </div>
-
-      <div class="col-4">
-        Ratio: 4/3
-        <q-img
-          src="https://picsum.photos/500/300"
-          :ratio="4/3"
-        />
+      <div class="text-h6 q-mb-sm">
+        Selamat datang di Dashboard e-Program
       </div>
     </div>
   </div>
-  <div class="q-pa-md">
-    <div class="q-col-gutter-md row items-start">
-      <div class="col-4">
-        Ratio: 16/9
-        <q-img
-          src="https://picsum.photos/500/300"
-          :ratio="16/9"
-        />
-      </div>
 
-      <div class="col-4">
-        Ratio: 1
-        <q-img
-          src="https://picsum.photos/500/300"
-          :ratio="1"
-        />
-      </div>
 
-      <div class="col-4">
-        Ratio: 4/3
-        <q-img
-          src="https://picsum.photos/500/300"
-          :ratio="4/3"
-        />
+  <div class="row flex flex-center q-pa-sm">
+    <div class="col-4">
+      <q-img
+        src="lib/images/gambar-video-handal.png"
+        class="text-shadow"
+        style="width: 100%; border-radius: 20px;"
+      />
+    </div>
+    <div class="col-12 col-md-6 q-mt-xl q-pa-xl">
+      <div class="text-h5 text-bold q-mb-md" style="text-align: justify;">e-Program merupakan alat bantu dalam menyusun rencana program pembangunan jalan</div>
+      <div class="text-h7 q-my-md" style="text-align: justify;">Tersedia untuk meningkatkan efisiensi proses pengelolaan data geospasial, guna mendukung tata kelola ruang yang lebih baik, melalui platform arsitektur data untuk pengelolaan dan analisis spasial, yang dapat membantu dalam akuisisi dan pengelolaan data, hingga analisis dan visualisasi data</div>
+      <q-btn color="primary" label="Primary" />
+    </div>
+  </div>
+
+  <div class="row flex flex-center q-pa-sm" style="background-color: rgb(247, 247, 247);">
+    <div class="col-12 q-py-xl q-px-xl">
+      <div class="text-h5 text-bold">Mengapa e-Program</div>
+    </div>
+    <div class="col-12 col-md-6 responsive-padding q-py-md">
+      <div class="q-item q-item-type row no-wrap" role="listitem">
+        <div class="q-avatar rounded-borders q-mr-md">
+          <div class="q-avatar__content row flex-center overflow-hidden">
+            <img src="https://picsum.photos/500/300">
+          </div>
+        </div>
+        <div class="q-item__section column q-item__section--main justify-center">
+          <div class="q-item__label text-h6 text-bold">Peta Input Lokasi rencana pembangunan</div>
+          <div class="q-item__label text-h8" style="text-align: justify;">Peta Input Lokasi rencana pembangunan.</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-6 responsive-padding q-py-md">
+      <div class="q-item q-item-type row no-wrap" role="listitem">
+        <div class="q-avatar rounded-borders q-mr-md">
+          <div class="q-avatar__content row flex-center overflow-hidden">
+            <img src="https://picsum.photos/500/300">
+          </div>
+        </div>
+        <div class="q-item__section column q-item__section--main justify-center">
+          <div class="q-item__label text-h6 text-bold">Pembobotan Ruas rencana pembangunan</div>
+          <div class="q-item__label text-h8" style="text-align: justify;">Pembobotan Ruas rencana pembangunan.</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-6 responsive-padding q-pt-md q-pb-xl">
+      <div class="q-item q-item-type row no-wrap" role="listitem">
+        <div class="q-avatar rounded-borders q-mr-md">
+          <div class="q-avatar__content row flex-center overflow-hidden">
+            <img src="https://picsum.photos/500/300">
+          </div>
+        </div>
+        <div class="q-item__section column q-item__section--main justify-center">
+          <div class="q-item__label text-h6 text-bold">Manajemen data rencana pembangunan</div>
+          <div class="q-item__label text-h8" style="text-align: justify;">Manajemen data rencana pembangunan.</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-6 responsive-padding q-pt-md q-pb-xl">
+      <div class="q-item q-item-type row no-wrap" role="listitem">
+        <div class="q-avatar rounded-borders q-mr-md">
+          <div class="q-avatar__content row flex-center overflow-hidden">
+            <img src="https://picsum.photos/500/300">
+          </div>
+        </div>
+        <div class="q-item__section column q-item__section--main justify-center">
+          <div class="q-item__label text-h6 text-bold">Skenario dan exercise program pembangunan</div>
+          <div class="q-item__label text-h8" style="text-align: justify;">Skenario dan exercise program pembangunan.</div>
+        </div>
       </div>
     </div>
   </div>
-  <div class="q-pa-md">
-    <div class="q-col-gutter-md row items-start">
-      <div class="col-4">
-        Ratio: 16/9
-        <q-img
-          src="https://picsum.photos/500/300"
-          :ratio="16/9"
-        />
-      </div>
 
-      <div class="col-4">
-        Ratio: 1
-        <q-img
-          src="https://picsum.photos/500/300"
-          :ratio="1"
-        />
-      </div>
-
-      <div class="col-4">
-        Ratio: 4/3
-        <q-img
-          src="https://picsum.photos/500/300"
-          :ratio="4/3"
-        />
-      </div>
+  <div class="row flex flex-center q-pa-sm">
+    <div class="col-12 q-py-xl q-px-xl column items-center">
+      <div class="text-h5 text-bold">Fitur e-Program</div>
     </div>
-  </div>
-  <div class="q-pa-md">
-    <div class="q-col-gutter-md row items-start">
-      <div class="col-4">
-        Ratio: 16/9
-        <q-img
-          src="https://picsum.photos/500/300"
-          :ratio="16/9"
-        />
+
+    <div class="row q-col-gutter-md justify-center">
+      <div class="col-12 col-sm-6 col-md-3">
+        <q-card flat bordered class="q-pa-md bg-white full-height">
+          <q-card-section class="text-center">
+            <div class="text-body2">
+              Peta Input Lokasi<br>rencana pembangunan
+            </div>
+          </q-card-section>
+        </q-card>
       </div>
 
-      <div class="col-4">
-        Ratio: 1
-        <q-img
-          src="https://picsum.photos/500/300"
-          :ratio="1"
-        />
+      <div class="col-12 col-sm-6 col-md-3">
+        <q-card flat bordered class="q-pa-md bg-white full-height">
+          <q-card-section class="text-center">
+            <div class="text-body2">
+              Pembobotan Ruas<br>rencana pembangunan
+            </div>
+          </q-card-section>
+        </q-card>
       </div>
 
-      <div class="col-4">
-        Ratio: 4/3
-        <q-img
-          src="https://picsum.photos/500/300"
-          :ratio="4/3"
-        />
+      <div class="col-12 col-sm-6 col-md-3">
+        <q-card flat bordered class="q-pa-md bg-white full-height">
+          <q-card-section class="text-center">
+            <div class="text-body2">
+              Manajemen data<br>rencana pembangunan
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <div class="col-12 col-sm-6 col-md-3">
+        <q-card flat bordered class="q-pa-md bg-white full-height">
+          <q-card-section class="text-center">
+            <div class="text-body2">
+              Skenario dan exercise<br>program pembangunan
+            </div>
+          </q-card-section>
+        </q-card>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
+export default {
   name: 'IndexPage'
-})
+}
 </script>
+
+<style>
+</style>
